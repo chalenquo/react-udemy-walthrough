8df7e128150c7966478c4e210772645caa532562
Sergii Chalenko <chalenko.dev@gmail.com>
2022-05-25 10:38:21 +0300

    Lesson 23. Video attempts

---
+++ js/lesson23.js
@@ -0,0 +1,33 @@
+//
+//
+// let result = '';
+// const  length = 5;
+//
+//
+// for (let i = 1; i < length; i++) {
+//
+//     for (let j = 0; j < i; j++) {
+//         result += '*'
+//     }
+//
+//     result += '\n'
+// }
+//
+// console.log(result)
+//
+
+
+// first: for (let i = 1; i < 3; i++) {
+//     console.log(`1st level ${i}`)
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`2nd level ${j}`)
+//         for (let k = 0; k < 3; k++) {
+//             if (k === 2) break first;
+//             console.log(`3rd level ${k}`)
+//         }
+//     }
+// }
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`1st level ${i}`)
+}
