28f0090d86638f4aaa2e4d925d3e482cd4767dac
Sergii Chalenko <chalenko.dev@gmail.com>
2022-05-25 15:09:24 +0300

    Lesson 29. Cinema

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 "use strict";
 
-let numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,23 +19,52 @@ const personalMovieDB = {
     private: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Який останній фільм бачили?', ''),
-          b = +prompt('На скільки його оціните? (0.0-10.0)', '');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Який останній фільм бачили?', ''),
+            b = +prompt('На скільки його оціните? (0.0-10.0)', '');
 
-    if (a != null && b != null && a !== '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done')
-    } else {
-        console.log('error')
-        i--
+        if (a != null && b != null && a !== '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done')
+        } else {
+            console.log('error')
+            i--
+        }
     }
 }
 
-let status = (personalMovieDB.count <= 10) ? 'Мало фільмів'
-           : (personalMovieDB.count > 10 && personalMovieDB.count < 30) ? 'Класичний глядач'
-           : 'Кіноман';
+rememberMyFilms()
+
+function detectPersonaLevel() {
+    let level = (personalMovieDB.count <= 10) ? 'Мало фільмів'
+              : (personalMovieDB.count > 10 && personalMovieDB.count < 30) ? 'Класичний глядач'
+              : 'Кіноман';
+    console.log(level);
+    return level;
+}
+
+detectPersonaLevel()
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres.push( prompt(`Ваш улюблений жанр ${i}`, '') );
+    }
+    console.log(personalMovieDB.genres)
+}
+
+writeYourGenres();
 
 
-console.log(personalMovieDB);
-console.log(status)
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    } else {
+        console.log('Прихована');
+    }
+}
+
+showMyDB(personalMovieDB.private);
+
+
+
