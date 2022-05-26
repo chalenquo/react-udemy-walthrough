9339dd840ee5bd8262d5dc9e5247b26429bdef68
Sergii Chalenko <chalenko.dev@gmail.com>
2022-05-26 16:21:05 +0300

    Lesson 31. Task 1

---
+++ js/lesson31_task1.js
@@ -0,0 +1,34 @@
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+};
+
+function isBudgetEnough(data) {
+    let fullVolume = 0;
+    data.shops.forEach((item, index) => {
+        fullVolume += item.width * item.length * data.height;
+    });
+    const bugdetNeed = fullVolume * data.moneyPer1m3;
+    return (bugdetNeed > data.budget) ? 'Бюджета недостаточно' : 'Бюджета достаточно';
+}
+
+console.log(isBudgetEnough(shoppingMallData))
