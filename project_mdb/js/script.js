55892bc93d8f17b280880de35d2cb24914538daf
Sergii Chalenko <chalenko.dev@gmail.com>
2022-05-29 16:34:08 +0300

    Lesson 44. project_mdb

--- project_mdb/js/script.js
+++ project_mdb/js/script.js
@@ -24,3 +24,31 @@ const movieDB = {
     ]
 };
 
+const ads = document.querySelector('.promo__adv');
+const bg = document.querySelector('.promo__bg');
+const genre = bg.querySelector('.promo__genre');
+const promoList = document.querySelector('.promo__interactive-list')
+const promoItem = promoList.querySelector('.promo__interactive-item')
+const del = promoItem.querySelector('.delete')
+
+promoList.innerHTML = '';
+movieDB.movies.sort()
+
+movieDB.movies.forEach((film, i) => {
+    promoList.innerHTML += `
+    <li class="promo__interactive-item">${i+1}. ${film}
+        <div class="delete"></div>
+    </li>
+    `
+})
+
+
+
+
+
+
+
+
+ads.innerHTML = '';
+bg.style.backgroundImage = 'url("img/bg.jpg")'
+genre.textContent = 'ДРАМА';
