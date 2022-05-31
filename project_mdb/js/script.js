94d9a46ac438fff6b7c4da64cca419e18dd34d03
Sergii Chalenko <chalenko.dev@gmail.com>
2022-05-31 11:49:16 +0300

    Lesson 47. Cinema

--- project_mdb/js/script.js
+++ project_mdb/js/script.js
@@ -1,54 +1,64 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+    const ads = document.querySelector('.promo__adv');
+    const bg = document.querySelector('.promo__bg');
+    const genre = bg.querySelector('.promo__genre');
+    const promoList = document.querySelector('.promo__interactive-list')
+    const promoItem = promoList.querySelector('.promo__interactive-item')
+    const del = promoItem.querySelector('.delete')
 
-const ads = document.querySelector('.promo__adv');
-const bg = document.querySelector('.promo__bg');
-const genre = bg.querySelector('.promo__genre');
-const promoList = document.querySelector('.promo__interactive-list')
-const promoItem = promoList.querySelector('.promo__interactive-item')
-const del = promoItem.querySelector('.delete')
+    promoList.innerHTML = '';
+    movieDB.movies.sort()
 
-promoList.innerHTML = '';
-movieDB.movies.sort()
-
-movieDB.movies.forEach((film, i) => {
-    promoList.innerHTML += `
+    movieDB.movies.forEach((film, i) => {
+        promoList.innerHTML += `
     <li class="promo__interactive-item">${i+1}. ${film}
         <div class="delete"></div>
     </li>
     `
+    })
+
+    ads.innerHTML = '';
+    bg.style.backgroundImage = 'url("img/bg.jpg")'
+    genre.textContent = 'ДРАМА';
+
+    const input = document.querySelector('.adding__input')
+    const form = document.querySelector('.add')
+    const btn = form.querySelector('button')
+    const chk = form.querySelector('[type="checkbox"]')
+    console.log(input.value);
+
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        e.stopPropagation()
+
+        let movie = input.value;
+        if (movie.length >= 21) {
+            movie = movie.slice(0, 21) + '...';
+        }
+        movieDB.movies.push(movie);
+        if (chk.checked) {
+            console.log(movie)
+        }
+        promoList.innerHTML += `
+    <li class="promo__interactive-item">${movieDB.movies.length}. ${movie}
+        <div class="delete"></div>
+    </li>
+    `
+        movieDB.movies.sort();
+    })
+
 })
 
 
 
-
-
-
-
-
-ads.innerHTML = '';
-bg.style.backgroundImage = 'url("img/bg.jpg")'
-genre.textContent = 'ДРАМА';
