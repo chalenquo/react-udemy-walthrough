01299acf86a85e30f69745f98c52a59f5411c492
Sergii Chalenko <chalenko.dev@gmail.com>
2022-06-01 14:57:13 +0300

    Lesson 59. Map()

--- js/tests.js
+++ js/tests.js
@@ -19,6 +19,52 @@
 // 1 - через Array.slice()
 // 2 - через спред оператори [...array]
 
-let y = 1;
-let x = y = 2
-console.log(2 && 1 && null && 0 && undefined)
+// let y = 1;
+// let x = y = 2
+// console.log(2 && 1 && null && 0 && undefined)
+
+const obj = {
+    name: 'Sergii',
+    age: 34,
+    car: {
+        brand: 'Opel',
+        model: 'Corsa',
+        engine: 1.4
+    },
+    block: function () {
+        Object.defineProperties(this, {
+            name: {
+                writable: false
+            },
+            age: {
+                writable: false
+            }
+        })
+
+    },
+    clone: function () {
+        const objMap = new Map(Object.entries(this))
+        return Object.fromEntries(objMap)
+    }
+}
+
+let clone = obj.clone();
+clone.car.brand = 'BMW'
+console.log(obj)
+console.log(clone);
+
+
+// const clone = Object.assign({}, obj)
+// clone.name = 'Ostap'
+// clone.car.brand = 'Audi'
+// console.log(clone)
+// console.log(obj)
+
+// const objMap = new Map(Object.entries(obj))
+// const newObj = Object.fromEntries(objMap)
+
+
+// newObj.name = 'Woo'
+// console.log(objMap)
+// console.log(obj)
+// console.log(newObj)
